3f1e8b1af878b0a880f6059145e16215634ef399
Danish khan <danish230296@gmail.com>
2023-03-20 21:04:57 +0400

    Initial Commit Setting Up Project
    (cherry picked from commit 94c4109a566c416aa059ea9b8a729f6d0f637d68)

---
+++ src/main/java/com/ticketingsystem/automatedticketsystem/AutomatedTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingsystem.automatedticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTicketSystemApplication.class, args);
+	}
+
+}
