94c4109a566c416aa059ea9b8a729f6d0f637d68
Danish khan <danish230296@gmail.com>
2023-03-20 21:04:57 +0400

    Initial Commit Setting Up Project

---
+++ src/test/java/com/ticketingsystem/automatedticketsystem/AutomatedTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketingsystem.automatedticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
